771817b25ae58fec16db3c44ab73ce9a95f21dc4
Connor Leech <connorleech@gmail.com>
2014-08-28 02:57:02 -0700

    bugfix

--- utils/summary_totals/contByTime.js
+++ utils/summary_totals/contByTime.js
@@ -3,9 +3,8 @@
 
 var path = require('path'),
 	amount = 1000,
-	summaryUrl = require(path.join(__dirname, '../../data/Campaign_Finance_-_FPPC_Form_460_-_Summary_Totals.csv')),
+	summaryUrl = path.join(__dirname, '../../data/Campaign_Finance_-_FPPC_Form_460_-_Summary_Totals.csv'),
 	filterContributions = require(path.join(__dirname, '../readAndFilter.js'));
-
 
 // check if contribution amount is greater than given value
 var greaterThan = function(amount){
